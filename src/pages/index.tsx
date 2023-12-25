60e541dcebc070e35120387dd5801b9de2fbbdba
VadimChorrny <65212516+VadimChorrny@users.noreply.github.com>
2023-12-25 20:52:00 +0200

    feature: rich text editor, ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,111 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import {Inter} from 'next/font/google'
+import Head from "next/head";
+import {useEffect, useState} from "react";
+import dynamic from "next/dynamic";
+import 'react-quill/dist/quill.snow.css';
+import {toast} from "react-toastify";
+import hljs from "highlight.js";
+import 'highlight.js/styles/atom-one-dark.css';
 
-const inter = Inter({ subsets: ['latin'] })
+
+const ReactQuill = dynamic(() => import('react-quill'), {ssr: false});
+
+const inter = Inter({subsets: ['latin']})
 
 export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    const [value, setValue] = useState<string>('');
+    const [showResult, setShowResult] = useState<boolean>(false);
+    const notify = () => toast("Copied to clipboard!");
+    const handleCopyCode = async () => {
+        await navigator.clipboard.writeText(value);
+        notify();
+    }
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    useEffect(() => {
+        // Застосування підсвічування коду до всіх блоків <code>
+        document.querySelectorAll('pre').forEach((block) => {
+            hljs.highlightBlock(block as HTMLElement);
+        });
+    }, [value]);
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    return (
+        <>
+            <Head>
+                <title>Content Converter</title>
+                <meta name="description"
+                      content="An enticing tool for instantly converting text into HTML: Content Converter. Edit and generate HTML effortlessly!"/>
+                <link rel="icon" href="/favicon/favicon.ico"/>
+            </Head>
+            <h1 className={"text-4xl font-bold text-center mt-24 "}>
+                Content Converter -
+                <span className={"text-blue-500"}>
+                    {" HTML"}
+                </span> Generator 📄
+            </h1>
+            <main
+                className={`flex min-h-screen flex-col items-center p-24 ${inter.className}`}
+            >
+                <section className="w-full">
+                    <h2 className="text-2xl font-bold mb-2">
+                        Enter your content
+                    </h2>
+                    <ReactQuill
+                        theme="snow"
+                        value={value}
+                        onChange={setValue}
+                        modules={{
+                            toolbar: [
+                                [{'header': [1, 2, 3, 4, 5, 6, false]}],
+                                [{'font': []}],
+                                ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+                                // code-block
+                                ['code-block'],
+                                [{'color': []}, {'background': []}],
+                                [{'script': 'sub'}, {'script': 'super'}],
+                                [{'align': []}],
+                                [{'indent': '-1'}, {'indent': '+1'}],
+                                [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
+                                ['link', 'image', 'video'],
+                                ['clean']
+                            ]
+                        }}
+                        placeholder="Enter your content here..."
+                    />
+                </section>
+                <section className={`w-full mt-24 ${inter.className}`}>
+                    <h2 className="text-2xl font-bold mb-2">
+                        HTML code
+                    </h2>
+                    <div className={'w-full border-2 border-gray-300 p-4'}>
+                        <code>
+                            {value}
+                        </code>
+                    </div>
+                    <button className={'mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'}
+                            disabled={!value}
+                            onClick={handleCopyCode}>
+                        Copy
+                    </button>
+                </section>
+                <section className={`w-full mt-24 ${inter.className}`}>
+                    <p>
+                        <span className={'text-blue-500 cursor-pointer hover:underline'}
+                              onClick={() => setShowResult(!showResult)}>
+                            {showResult ? 'Hide the rendered HTML' : 'Click here to see the rendered HTML'}
+                        </span>
+                    </p>
+                    {showResult && <div className={'w-full border-2 border-gray-300 p-4 mt-4'}
+                                        dangerouslySetInnerHTML={{__html: value}}/>}
+                </section>
+            </main>
+            <footer className={"flex justify-center items-center h-24 bg-gray-100"}>
+                <div className={"text-center"}>
+                    <a href={"https://vadym-chornyi-website.vercel.app"}
+                       target={"_blank"} className={"hover:underline"}>
+                        This website was created by Vadym Chornyi © {new Date().getFullYear()}
+                    </a>
+                </div>
+            </footer>
+        </>
+    )
 }
